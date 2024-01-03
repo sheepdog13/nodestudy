5aa07cbcc833596fd48dee27ebef713b21644e2d
sheepdog13 <mikecho1234@naver.com>
2024-01-04 04:24:03 +0900

    feat : register post 요청 bodyparser로 req.body 데이터 가져와서 mongodb의 usermodel에 save postman으로 post요청

--- server/index.js
+++ server/index.js
@@ -1,6 +1,14 @@
 const express = require("express");
 const app = express();
 const port = 5000;
+const bodyParser = require("body-parser");
+const { User } = require("./models/User");
+
+// application/x-www-form-urlencoded 이렇게된 데이터를 분석해서 가져올 수 있게 해준다.
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// application/json 데이터를 분석해서 가져올 수 있게 해준다
+app.use(bodyParser.json());
 
 const mongoose = require("mongoose");
 mongoose
@@ -11,5 +19,24 @@ mongoose
   .catch((err) => console.log(err));
 
 app.get("/", (req, res) => res.send("hi"));
+
+app.post("/register", async (req, res) => {
+  //회원가입시 필요 정보를 client에서 가져오면
+  //데이터베이스에 삽입한다
+  //body parser를 통해 body에 담긴 정보를 가져온다
+  const user = new User(req.body);
+
+  //mongoDB 메서드, user모델에 저장
+  const result = await user
+    .save()
+    .then(() => {
+      res.status(200).json({
+        success: true,
+      });
+    })
+    .catch((err) => {
+      res.json({ success: false, err });
+    });
+});
 
 app.listen(port, () => console.log(`app lisening on port ${port}`));
