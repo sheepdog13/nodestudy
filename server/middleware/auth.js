1d54effa81171224c43279c01ae97fe1f9ae9a6c
sheepdog13 <mikecho1234@naver.com>
2024-02-04 14:54:37 +0900

    feat : access token 유효할때 #26 - access token 유효할때 access token 복호화후 - user정보 전달

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -3,6 +3,13 @@ const { User } = require("../models/User");
 let auth = async (req, res, next) => {
   // 클라이언트에서 토큰을 가져온다
   let token = req.headers.authorization;
+  if (token) {
+    const userdata = await User.findByToken(token);
+    req.user = userdata;
+    return next();
+  } else {
+    // refresh 토큰으로 access 토큰 재발급
+  }
   try {
     // 토큰을 복호화 한후 유저를 찾는다.
     const userdata = await User.findByToken(token);
