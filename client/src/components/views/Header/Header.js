2e7b3151ee3783cd00a3aa4a292f18f2da58c9a5
sheepdog13 <mikecho1234@naver.com>
2024-02-13 14:13:17 +0900

    fix : 안쓰는 component 삭제

--- client/src/components/views/Header/Header.js
+++ client/src/components/views/Header/Header.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import styled from "styled-components";
 import SvgIcon from "@mui/material/SvgIcon";
 import NightlightIcon from "@mui/icons-material/Nightlight";
