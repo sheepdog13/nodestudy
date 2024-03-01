5b1a74b642be2d5755d3218dd1ad65025b879a45
sheepdog13 <mikecho1234@naver.com>
2024-03-01 14:40:23 +0900

    fix : og img 태그 경로 변경

--- client/src/components/common/MetaTag.js
+++ client/src/components/common/MetaTag.js
@@ -6,6 +6,7 @@ function MetaTag(props) {
   return type === "home" ? (
     <Helmet>
       <title>sheepdog13blog</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
       <meta
         name="description"
         content="자바스크립트를 주로 다루는 기술 블로그입니다."
@@ -18,19 +19,23 @@ function MetaTag(props) {
         property="og:description"
         content="자바스크립트를 주로 다루는 기술 블로그입니다."
       />
-      <meta property="og:image" content="/img/bond.webp" />
+      <meta
+        property="og:image"
+        content="https://sheepdog13.blog/img/bond.webp"
+      />
       <meta property="og:url" content="https://sheepdog13.blog" />
     </Helmet>
   ) : (
     <Helmet>
       <title>{title}</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
       <meta name="description" content={desc} />
       <meta name="keywords" content="post" />
       <meta property="og:type" content="website" />
       <meta property="og:title" content={title} />
       <meta property="og:site_name" content={title} />
       <meta property="og:description" content={desc} />
-      <meta property="og:image" content={`${img}`} />
+      <meta property="og:image" content={`https://sheepdog13.blog${img}`} />
       <meta property="og:url" content={window.location.href} />
     </Helmet>
   );
